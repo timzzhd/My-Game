d34dc05a46b724102b8d110693ea71d9648534a7
timzzhd <74539817+timzzhd@users.noreply.github.com>
2020-11-23 23:29:12 +0000

    Movement

--- Oluwatimileyin Akande - My Personal Project/Assets/Scripts/PlayerContoller.cs
+++ Oluwatimileyin Akande - My Personal Project/Assets/Scripts/PlayerContoller.cs
@@ -7,21 +7,36 @@ public class PlayerContoller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    private float speed = 20.0f;
-    private float turnSpeed = 45.0f;
-    private float horizontalInput;
-    private float forwardInput;
+
+    public float horizontalInput;
+    public float speed = 15.0f;
+    public float xRange = 5;
+    public GameObject projectilePrefab;
+
+
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if (transform.position.x < xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
-    
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+
+        }
+
     }
 }
