0dac3ac825a5a3877fae5866f6b7fb4823c10dc0
timzzhd <74539817+timzzhd@users.noreply.github.com>
2020-11-26 00:39:06 +0000

    random spawn

---
+++ Oluwatimileyin Akande - My Personal Project/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private float topBound = 40;
+    private float lowerBound = -40;
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
--- Oluwatimileyin Akande - My Personal Project/Assets/Scripts/SpawnManager.cs
+++ Oluwatimileyin Akande - My Personal Project/Assets/Scripts/SpawnManager.cs
@@ -12,8 +12,25 @@ public class SpawnManager : MonoBehaviour
 
     // Update is called once per frame
     public GameObject[] objectPrefabs;
+    private float spawnRangeX = 0;
+    private float spawnPosZ = 0;
+
+
+
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S)) {
+           
+            // Randomly generate animal index and spawn position
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
+            0, spawnPosZ);
+
+            int objectIndex = Random.Range(0, objectPrefabs.Length);
+
+            Instantiate(objectPrefabs[objectIndex], spawnPos, objectPrefabs[objectIndex].transform.rotation);
+
+        }
+
     }
 }
