152d65cc288361d44fbe54ea424b44074773353d
timzzhd <74539817+timzzhd@users.noreply.github.com>
2020-12-20 19:35:11 +0000

    Music/Animations

---
+++ Oluwatimileyin Akande - My Personal Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour {
+
+
+	
+
+	public void Start()
+	{
+		
+	}
+	
+
+	 void Update()
+	{
+
+	}
+
+
+}
